bb6bf8590847dc93d0079c0c0d13ad5bd16337ec
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-16 01:26:40 -0300

    fire test with simple GET

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 3001
 
 app.use(cors())
 
+// fire test
+app.get('/', (_req, res) => res.send('Residencial app is on!'))
+
 app.use('/room', Controller)
 
 app.listen(port, () => console.log(`Running at ${port}`))
